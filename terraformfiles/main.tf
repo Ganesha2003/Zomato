ea9f4f1d5f13a53c042ef3f9102a2fc98903a6b2
Ganesha2003 <115620909+Ganesha2003@users.noreply.github.com>
2025-11-22 21:43:32 +0530

    Update main.tf

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -1,20 +1,28 @@
+provider "aws" {
+  region = "ap-south-1"   
+}
+
 resource "aws_instance" "test-server" {
   ami                    = "ami-0d176f79571d18a8f"          
   instance_type          = "t3.small"
-  vpc_security_group_ids = ["sg-01339d701b90ce090"]         
+  vpc_security_group_ids = ["sg-01339d701b90ce090"]        
 
-  
+  # Automatically configure server at launch
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y docker
+    systemctl start docker
+    systemctl enable docker
+    docker run -d -p 80:80 rmganesha/zomatoapp:1.0
+  EOF
+
   tags = {
     Name = "test-server"
   }
 
-  
+  # Save public IP to inventory file
   provisioner "local-exec" {
     command = "echo ${aws_instance.test-server.public_ip} > inventory"
-  }
-
-  
-  provisioner "local-exec" {
-    command = "ansible-playbook /var/lib/jenkins/workspace/zomatoapp/terraformfiles/ansiblebook.yml"
   }
 }
