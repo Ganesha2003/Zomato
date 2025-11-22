e206407b3ee389ffc930a216ea607c31b97579c7
Ganesha2003 <115620909+Ganesha2003@users.noreply.github.com>
2025-11-22 21:34:56 +0530

    Update main.tf

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -3,18 +3,17 @@ resource "aws_instance" "test-server" {
   instance_type          = "t3.small"
   vpc_security_group_ids = ["sg-01339d701b90ce090"]         
 
-  # Removed key_name and SSH connection since no key pair is used
-
+  
   tags = {
     Name = "test-server"
   }
 
-  # Save public IP to inventory file
+  
   provisioner "local-exec" {
     command = "echo ${aws_instance.test-server.public_ip} > inventory"
   }
 
-  # Run Ansible playbook locally (not over SSH)
+  
   provisioner "local-exec" {
     command = "ansible-playbook /var/lib/jenkins/workspace/zomatoapp/terraformfiles/ansiblebook.yml"
   }
