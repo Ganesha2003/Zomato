cf120b9b1e80746d171fc4f03d29244792a5a1bf
Ganesha2003 <115620909+Ganesha2003@users.noreply.github.com>
2025-11-22 20:44:19 +0530

    Update main.tf

--- terraformfiles/main.tf
+++ terraformfiles/main.tf
@@ -1,33 +1,27 @@
-resource "aws_instance" "test-server" {
-    ami                    = "ami-0d176f79571d18a8f"
-    instance_type          = "t3.small"
-    key_name               = "bookmyshow"
-    vpc_security_group_ids = ["sg-01339d701b90ce090"]
+provider "aws" {
+  region = "ap-south-1"   
+}
 
-    connection {
-        type        = "ssh"
-        user        = "ec2-user"
-        private_key = file("./bookmyshow .pem")
-        host        = self.public_ip
-    }
+variable "pem_file" {
+  description = "Path to the PEM file injected by Jenkins"
+  type        = string
+}
 
-    provisioner "remote-exec" {
-        inline = ["echo 'wait to start the instance' "]
-    }
+resource "aws_instance" "web" {
+  ami           = "ami-0d176f79571d18a8f" 
+  instance_type = "t3.small"              
+  key_name      = "bookmyshow .pem"       
 
-    tags = {
-        Name = "test-server"
-    }
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"                
+    private_key = file(var.pem_file)
+    host        = self.public_ip
+  }
 
-    provisioner "local-exec" {
-        # This writes the IP to the inventory file
-        command = "echo ${aws_instance.test-server.public_ip} > inventory"
-    }
-
-    # --------------------------------------------------------------------------
-    # CRITICAL FIX: Explicitly specify the inventory and the private key for Ansible
-    # --------------------------------------------------------------------------
-    provisioner "local-exec" {
-        command = "ansible-playbook -i inventory --private-key bookmyshow .pem /var/lib/jenkins/workspace/zomatoapp/terraformfiles/ansiblebook.yml"
-    }
+  provisioner "remote-exec" {
+    inline = [
+      "echo Hello from Terraform via Jenkins!"
+    ]
+  }
 }
